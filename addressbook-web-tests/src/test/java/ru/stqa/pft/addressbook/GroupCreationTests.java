386091154b4a949002582f46ace7c44ae52e3ae8
Aleksey Shishkin <aleksey.shishkin@mail.com>
2017-09-05 21:12:52 +0300

    Удаление групп

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/GroupCreationTests.java
@@ -1,79 +1,8 @@
 package ru.stqa.pft.addressbook;
 
 import org.testng.annotations.Test;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.AfterMethod;
 
-import static org.hamcrest.CoreMatchers.is;
-import static org.hamcrest.core.IsNot.not;
-
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.Dimension;
-import org.openqa.selenium.JavascriptExecutor;
-
-import java.util.*;
-
-public class GroupCreationTests {
-    private WebDriver driver;
-    private Map<String, Object> vars;
-    JavascriptExecutor js;
-
-    @BeforeMethod
-    public void setUp() {
-        System.setProperty("webdriver.gecko.driver", "/home/alex/IdeaProjects/java_pft/geckodriver");
-        driver = new FirefoxDriver();
-        js = (JavascriptExecutor) driver;
-        vars = new HashMap<String, Object>();
-
-        login("admin", "secret");
-    }
-
-    @AfterMethod
-    public void tearDown() {
-        driver.quit();
-    }
-
-    private void login(String username, String password) {
-        driver.get("http://192.168.1.240:8080/addressbook/");
-        driver.manage().window().setSize(new Dimension(1024, 744));
-        driver.findElement(By.name("user")).sendKeys(username);
-        driver.findElement(By.name("pass")).sendKeys(password);
-        driver.findElement(By.cssSelector("input:nth-child(7)")).click();
-    }
-
-
-    private void gotoGroupPage() {
-        driver.findElement(By.linkText("groups")).click();
-    }
-
-    private void initGroupCreation() {
-        driver.findElement(By.name("new")).click();
-    }
-
-    private void fillGroupForm(GroupData groupData) {
-        driver.findElement(By.name("group_name")).click();
-        driver.findElement(By.name("group_name")).clear();
-        driver.findElement(By.name("group_name")).sendKeys(groupData.getName());
-
-        driver.findElement(By.name("group_header")).click();
-        driver.findElement(By.name("group_header")).clear();
-        driver.findElement(By.name("group_header")).sendKeys(groupData.getHeader());
-
-        driver.findElement(By.name("group_footer")).click();
-        driver.findElement(By.name("group_footer")).clear();
-        driver.findElement(By.name("group_footer")).sendKeys(groupData.getFooter());
-    }
-
-    private void submitGroupCreation() {
-        driver.findElement(By.name("submit")).click();
-    }
-
-    private void returnToGroupPage() {
-        driver.findElement(By.linkText("group page")).click();
-    }
-
+public class GroupCreationTests extends TestBase {
 
     @Test
     public void newCat1() {
@@ -92,6 +21,4 @@ public class GroupCreationTests {
         submitGroupCreation();
         returnToGroupPage();
     }
-
-
 }
