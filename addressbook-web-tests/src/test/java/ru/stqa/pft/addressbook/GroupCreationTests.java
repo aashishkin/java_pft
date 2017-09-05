d7632c9d4e990e6841bd54bfeb9c527af724f74a
Aleksey Shishkin <aleksey.shishkin@mail.com>
2017-09-05 19:34:05 +0300

    Вынесена авторизация в @BeforeMethot

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/GroupCreationTests.java
@@ -47,6 +47,12 @@ public class GroupCreationTests {
     driver = new FirefoxDriver();
     js = (JavascriptExecutor) driver;
     vars = new HashMap<String, Object>();
+
+    driver.get("http://192.168.1.240:8080/addressbook/");
+    driver.manage().window().setSize(new Dimension(1024, 744));
+    driver.findElement(By.name("user")).sendKeys("admin");
+    driver.findElement(By.name("pass")).sendKeys("secret");
+    driver.findElement(By.cssSelector("input:nth-child(7)")).click();
   }
 
 //  @AfterTest
@@ -61,29 +67,19 @@ public class GroupCreationTests {
 
   @Test
   public void auth() {
-    driver.get("http://192.168.1.240:8080/addressbook/");
-    driver.manage().window().setSize(new Dimension(1024, 744));
-    driver.findElement(By.name("user")).sendKeys("admin");
-    driver.findElement(By.name("pass")).sendKeys("secret");
-    driver.findElement(By.cssSelector("input:nth-child(7)")).click();
     driver.findElement(By.linkText("groups")).click();
     driver.findElement(By.name("new")).click();
     driver.findElement(By.name("group_name")).click();
-    driver.findElement(By.name("group_name")).sendKeys("ВТБ Л");
+    driver.findElement(By.name("group_name")).sendKeys("Категория 1");
     driver.findElement(By.name("submit")).click();
   }
 
 @Test
   public void newCat() {
-    driver.get("http://192.168.1.240:8080/addressbook/");
-    driver.manage().window().setSize(new Dimension(1024, 744));
-    driver.findElement(By.name("user")).sendKeys("admin");
-    driver.findElement(By.name("pass")).sendKeys("secret");
-    driver.findElement(By.cssSelector("input:nth-child(7)")).click();
     driver.findElement(By.linkText("groups")).click();
     driver.findElement(By.name("new")).click();
     driver.findElement(By.name("group_name")).click();
-    driver.findElement(By.name("group_name")).sendKeys("ВТБ Ф");
+    driver.findElement(By.name("group_name")).sendKeys("Категория 2");
     driver.findElement(By.name("submit")).click();
   }
 }
