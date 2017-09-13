897b379527e514fbd5d42ae95d2534549f711c69
Aleksey Shishkin <aleksey.shishkin@mail.com>
2017-09-13 22:35:09 +0300

    Проверка номеров телефонов (разрезание строк, 57)

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactPhoneTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactPhoneTests.java
@@ -1,7 +1,12 @@
 package ru.stqa.pft.addressbook.tests;
 
+import org.hamcrest.CoreMatchers;
+import org.hamcrest.MatcherAssert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
+
+import static org.hamcrest.CoreMatchers.*;
+import static org.hamcrest.MatcherAssert.*;
 
 public class ContactPhoneTests extends TestBase {
 
@@ -10,9 +15,15 @@ public class ContactPhoneTests extends TestBase {
         app.goTo().gotoHomePage();
         ContactData contact = app.contact().all().iterator().next();
         ContactData contactInfoFromEditForm = app.contact().infoFromEditForm(contact);
-        System.out.println(contactInfoFromEditForm.getHomePhone());
-        System.out.println(contactInfoFromEditForm.getWorkPhone());
-        System.out.println(contactInfoFromEditForm.getMobile());
 
+        assertThat(contact.getHomePhone(), equalTo(cleaned(contactInfoFromEditForm.getHomePhone())));
+        assertThat(contact.getWorkPhone(), equalTo(cleaned(contactInfoFromEditForm.getWorkPhone())));
+        assertThat(contact.getMobile(), equalTo(cleaned(contactInfoFromEditForm.getMobile())));
+
+    }
+
+    public String cleaned(String phone){
+        return phone.replaceAll("\\s", "").
+                replaceAll("[-()]", "");
     }
 }
