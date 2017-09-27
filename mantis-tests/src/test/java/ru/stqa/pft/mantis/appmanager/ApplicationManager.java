448e38934e8729a8f31060a213b4b020099887a5
Aleksey Shishkin <aleksey.shishkin@mail.com>
2017-09-27 23:53:04 +0300

    Подмена конфигурационного файла Mantis по FTP (80)

--- mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/ApplicationManager.java
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/ApplicationManager.java
@@ -16,6 +16,7 @@ public class ApplicationManager {
     private WebDriver driver;
     private String browser;
     private RegistrationHelper registrationHelper;
+    private FtpHelper ftp;
 
     public ApplicationManager(String browser) {
 
@@ -37,6 +38,13 @@ public class ApplicationManager {
             registrationHelper =  new RegistrationHelper(this);
         }
         return registrationHelper;
+    }
+
+    public FtpHelper ftp() {
+        if (ftp == null) {
+            ftp = new FtpHelper(this);
+        }
+    return ftp;
     }
 
     public String getProperty(String key) {
--- mantis-tests/src/test/java/ru/stqa/pft/mantis/tests/TestBase.java
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/tests/TestBase.java
@@ -4,6 +4,8 @@ import org.openqa.selenium.remote.BrowserType;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
 import ru.stqa.pft.mantis.appmanager.ApplicationManager;
+
+import java.io.File;
 import java.io.IOException;
 
 public class TestBase {
@@ -16,10 +18,12 @@ public class TestBase {
     @BeforeSuite
     public void setUp() throws IOException {
         app.init();
+        app.ftp().upload(new File("src/test/resources/config_inc.php"), "storage/sdcard0/htdocs/mantis/config/config_inc.php", "storage/sdcard0/htdocs/mantis/config/config_inc.php.bak");
     }
 
     @AfterSuite(alwaysRun = true)
-    public void tearDown() {
+    public void tearDown() throws IOException {
+        app.ftp().restore("storage/sdcard0/htdocs/mantis/config/config_inc.php.bak", "storage/sdcard0/htdocs/mantis/config/config_inc.php");
         app.stop();
     }
 
