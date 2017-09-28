b28a7ff11958342128c6cf0da87f93ca822594fb
Aleksey Shishkin <aleksey.shishkin@mail.com>
2017-09-28 23:01:13 +0300

    Заполнение формы регистрации (81)

--- mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/RegistrationHelper.java
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/RegistrationHelper.java
@@ -1,17 +1,18 @@
 package ru.stqa.pft.mantis.appmanager;
 
-import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.By;
 
-public class RegistrationHelper {
-    private final ApplicationManager app;
-    private WebDriver driver;
+public class RegistrationHelper extends HelperBase {
 
-    public RegistrationHelper(ApplicationManager app) {
-        this.app = app;
-        driver = app.getDriver();
+        public RegistrationHelper(ApplicationManager app) {
+        super(app);
     }
 
     public void start(String username, String email) {
         driver.get(app.getProperty("web.baseUrl") + "/signup_page.php");
+        type(By.name("username"), username);
+        type(By.name("email"), email);
+        click(By.cssSelector("input[value='Signup']"));
+
     }
 }
