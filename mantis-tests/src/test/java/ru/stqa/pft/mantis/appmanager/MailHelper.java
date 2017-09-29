63c4256f3566397d85ba3d669c259a38fa2443ca
Aleksey Shishkin <aleksey.shishkin@mail.com>
2017-09-29 23:52:47 +0300

    Настройка почтового сервера

--- mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/MailHelper.java
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/MailHelper.java
@@ -18,6 +18,7 @@ public class MailHelper {
   public MailHelper(ApplicationManager app) {
     this.app = app;
     wiser = new Wiser();
+    wiser.setPort(9595);
   }
 
   public List<MailMessage> waitForMail(int count, long timeout) throws MessagingException, IOException {
--- mantis-tests/src/test/resources/config_inc.php
+++ mantis-tests/src/test/resources/config_inc.php
@@ -8,5 +8,6 @@ $g_signup_use_captcha     = OFF;
 $g_default_timezone       = 'Europe/Moscow';
 $g_phpMailer_method = PHPMAILER_METHOD_SMTP;
 $g_smtp_host = '192.168.1.31';
+$g_smtp_port = 9595;
 
 $g_crypto_master_salt     = 'hxruNQCQlC55MG9dD0k30We/vecBWoU87OZsEt0CnCI=';
