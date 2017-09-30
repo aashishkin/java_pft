4bc4063476d05ca1fc14d0a540ecf38c7884d418
Aleksey Shishkin <aleksey.shishkin@mail.com>
2017-09-30 19:27:39 +0300

    Работа с github через API (88)

---
+++ github-sample/src/test/java/ru/stqa/pft/github/GithubTests.java
@@ -0,0 +1,19 @@
+package ru.stqa.pft.github;
+
+import com.google.common.collect.ImmutableMap;
+import com.jcabi.github.*;
+import org.testng.annotations.Test;
+
+import java.io.IOException;
+
+public class GithubTests {
+
+  @Test
+  public void testCommits() throws IOException {
+    Github github = new RtGithub("0d4cf1006fe86e5f5ae66dee890914d5ae36c7cf");
+    RepoCommits commits = github.repos().get(new Coordinates.Simple("aashishkin", "java_pft")).commits();
+    for (RepoCommit commit : commits.iterate(new ImmutableMap.Builder<String, String>().build())) {
+      System.out.println(new RepoCommit.Smart(commit).message());
+    }
+  }
+}
